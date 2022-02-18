a6880bcf2751e0574f4dcd56012d706ee225eab0
Florent Barre <florent.barre.pro@gmail.com>
2022-02-18 10:10:35 +0100

    app etape and ingredient

--- app.py
+++ app.py
@@ -15,14 +15,31 @@ def main():
         page_icon=None,  # String, anything supported by st.image, or None.
     )
 
+    # Load data
     LoadData.init_connection()
+    list_recette = ["Baguette", "Croissant"]
+
+    st.title("Bakery Sagna")
+
+    st.subheader("Choisir une recette")
+    recette = st.selectbox("Recette", list_recette)
+    st.number_input(f"Nombre de {recette}", min_value=1)
+
+    st.subheader("Liste des ingrédients")
+    list_ingredient = ["1 Oeuf", "100g de farine"]
+    for ingredient in list_ingredient:
+        st.write(f"- {ingredient}")
+
+    st.subheader("Etapes")
+    list_etapes = ["Etape 1 : Mélanger", "Etape 2 : Cuire"]
+    for etape in list_etapes:
+        st.write(f"- {etape}")
+
 
     # Bottom page
     st.write("\n")
     st.write("\n")
-    st.info("""By : [Linkedin](https://www.linkedin.com/in/florent-barre-a25921194/) / 
-            Ligue des Datas [Instagram](https://www.instagram.com/ligueddatas/)  |
-            Data source : [Twitter](https://twitter.com/home)""")
+    st.info("""By : [Linkedin](https://www.linkedin.com/in/florent-barre-a25921194/))""")
 
 
 if __name__ == "__main__":
